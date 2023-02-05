622ebebed37e112f2e6bd2a77f0c3ed9bc0e2e52
Rodrigo Figueira de Oliveira <oliveirafly@yahoo.com.br>
2023-02-05 16:38:47 -0300

    Add files via upload

---
+++ Calculadora-js/script.js
@@ -0,0 +1,26 @@
+function calculo(tipo, valor) {
+
+    if (tipo === 'acao') {
+
+        if (valor === 'C') {
+           //limpar o visor (id resultado)
+           document.getElementById('resultado').value = ''
+        }
+        
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+            document.getElementById('resultado').value += valor
+        }
+
+        if(valor === '=') {
+
+            let campo = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = campo
+        
+        }
+        
+    } else if (tipo === 'valor') {
+
+        document.getElementById('resultado').value += valor
+    }
+}
+
